40f5873b4c1b975fd5a723feeb6dbd55403656ae
mazen <mazen.snobar.snobar@hva.nl>
2025-02-24 01:57:02 +0100

    fixed errors

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const authOptions = {
+const authOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
